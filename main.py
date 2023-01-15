a76c7280c54f07de3ea09a65742086efab8d4484
Pedro Garcia <pedro.metalurgia@gmail.com>
2023-01-14 22:54:21 -0300

    update database january

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import mysql.connector
+import os
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -13,37 +14,19 @@ mycursor = mydb.cursor()
 def init_dataBase(tableName):
     mycursor.execute(
         "CREATE TABLE " + tableName + " (id INT AUTO_INCREMENT PRIMARY KEY,"
-        "date VARCHAR(255),"
-        "team VARCHAR(255)," 
+        "date VARCHAR(1255),"
+        "team VARCHAR(1255)," 
         "delivery INT,"
-        "strength VARCHAR(255),"
-        "improvment VARCHAR(255)," 
-        "felling VARCHAR(255),"
-        "evaluation VARCHAR(255))"
+        "strength VARCHAR(1255),"
+        "improvement VARCHAR(1255)," 
+        "felling VARCHAR(1255),"
+        "evaluation VARCHAR(1255))"
         )
 
 def add(val, tableName):
-    sql = "INSERT INTO " + tableName + " (date, team, delivery, strength, improvment, felling, evaluation) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO " + tableName + " (date, team, delivery, strength, improvement, felling, evaluation) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     mycursor.execute(sql, val)
     mydb.commit()
-    print(mycursor.rowcount, "record inserted.")
-
-def agilimToDB(path, csvName, tableName):
-    header = ['data', 'time', 'entrega', 'positivo', 'melhoria', 'humor', 'avaliacao']
-    with open(csvName, 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-
-        # write the header
-        writer.writerow(header)
-
-        # write the data
-        lines = [line for line in open(path)]
-
-        for i in range(1, len(lines)):
-            tupla = lines[i].strip().split(',')
-            dados = tupla[0].strip().split('       ')
-            writer.writerow(dados)
-            add(dados, tableName)
 
 def showInstances(tableName):
     mycursor.execute("SELECT * FROM " + tableName)
@@ -51,10 +34,34 @@ def showInstances(tableName):
     for x in myresult:
         print(x)
 
-def interationsToMySQL(csvOrigin, csvToStore, tableName): # ("instances", files\Agilim_Outubro.csv", "AgOutToDB.csv")
-    init_dataBase(tableName) 
-    agilimToDB(csvOrigin, csvToStore, tableName)
-    showInstances(tableName)
+def verifyIni(answer):
+    return ((answer[4] == "-") & (answer[7] == "-") & (answer[10] == "T") & (answer[13] == ":") &        
+            (answer[16] == ":") & (answer[19] == "Z"))
+
+def verifyEnd(answer):
+    return (((answer[len(answer)-1] == "1") & (answer[len(answer)-2] == ",") & 
+            (answer[len(answer)-3] == "%")))
+
+def deleteTableDB(tableName):
+    mycursor.execute("DROP TABLE " + tableName)
+    mydb.commit()
+
+def updateMySQL(fileCSV, tableName):
+    fileTest = open(fileCSV, encoding='utf-8', mode='r')
+    head = fileTest.readline()
+    content = fileTest.read()
+    answers = content.splitlines()
+    for ind in range(0, len(answers)-1):
+        if (verifyIni(answers[ind])): 
+            if (verifyEnd(answers[ind])):
+                dados = answers[ind].split('       ')
+                dados[6] = dados[6][:len(dados[6])-8]
+                add(dados, tableName)
+            else:
+                answers[ind+1] = answers[ind] + answers[ind + 1]
+                ind = ind + 1
 
 if __name__ == '__main__':
-  showInstances("answers")
+    init_dataBase("instances")
+    updateMySQL("./files/Agilim_Outubro.csv", "instances")
+    updateMySQL("./files/Agilim_Janeiro23_Quinzena01.csv", "instances")
