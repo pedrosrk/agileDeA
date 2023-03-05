07602de70305a97439412b9d7cace3ffe89ff9ab
Pedro Saint Clair Garcia <pedro.garcia@take.net>
2023-03-05 15:28:14 -0300

    dataBase OOP analyzes correlations

--- app.py
+++ app.py
@@ -4,28 +4,42 @@ from scipy import stats
 from mainDB import dataBase as db
 
 class dataAnalisis:
-  def __init__(self):
-    self.aglDB = db()
+  def __init__(self, text = "I love the computer sicence", model = "Default"):
+    self.aglDB = db() # Agilim's database
+    self.text = text
+    self.model = model
+
+  def getText(self):
+    return self.text
+  
+  def setText(self, message):
+    self.model = message
+
+  def getModel(self):
+    return self.model
+  
+  def setModel(self, model):
+    self.text = model
 
   def correlationFellingDeliveryScatter(self):
-    x_axis = self.aglDB.extractFellingList()
-    y_axis = self.aglDB.extractDeliveryList()
+    x_axis = self.aglDB.extractFellingList('instances')
+    y_axis = self.aglDB.extractDeliveryList('instances')
     x = np.array(x_axis)
     y = np.array(y_axis)
     plt.scatter(x, y)
     plt.show()
 
   def correlationFellingDeliveryBar(self):
-    x_axis = self.aglDB.extractFellingList()
-    y_axis = self.aglDB.extractDeliveryList()
+    x_axis = self.aglDB.extractFellingList('instances')
+    y_axis = self.aglDB.extractDeliveryList('instances')
     x = np.array(x_axis)
     y = np.array(y_axis)
     plt.bar(x,y)
     plt.show()
 
   def corrDeliveryAndFelling(self):
-    x = self.aglDB.extractDeliveryList()
-    y = self.aglDB.extractFellingListInt()
+    x = self.aglDB.extractDeliveryList('instances')
+    y = self.aglDB.extractFellingListInt('instances')
     slope, intercept, r, p, std_err = stats.linregress(x, y)
     def myfunc(x):
       return slope * x + intercept
@@ -37,6 +51,6 @@ class dataAnalisis:
     return r
 
 if __name__ == '__main__':
-  dados = db()
-  print(dados.extractDeliveryList('instances'))
+  dados = dataAnalisis()
+  dados.corrDeliveryAndFelling()
   
